1da91ff2c6f59058c8f3b4b575efac69bf1ac5f2
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-05-19 16:55:59 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/example/course/Entities/Order.java
+++ src/main/java/com/example/course/Entities/Order.java
@@ -3,6 +3,9 @@ package com.example.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,18 +16,20 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User user;
-
 
     public Order() {
     }
@@ -84,5 +89,4 @@ public class Order implements Serializable{
         return true;
     }
 
-    
 }
--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
